2cbfd0a6febd4d9c51ebd401a1d06ac13e9c144c
leonl <leonlanglang@gmail.com>
2018-06-28 20:46:25 +0200

    ddddddsss

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -26,6 +27,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.begin();
 		x++;
 		batch.draw(img,x, 0);
+		if(Gdx.graphics.getWidth() < x){
+			x=0;
+		}
 		batch.end();
 	}
 	//test
